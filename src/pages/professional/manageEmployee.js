c6256ce646963c6460c0b42cc4886aefbb9f6aa5
VincentPeer <vincent.peer@heig-vd.ch>
2022-08-26 17:39:42 +0200

    I feel it coming

--- src/pages/professional/manageEmployee.js
+++ src/pages/professional/manageEmployee.js
@@ -7,8 +7,15 @@ import DialogContentText from '@mui/material/DialogContentText';
 
 import { Typography, Button, Paper } from "@mui/material";
 
+const employee = {
+    name: ""
+};
 
-const Employee = () => {
+let text = "<ul>";
+
+const employeesList = []
+
+const EmployeeManagement = () => {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -18,54 +25,74 @@ const Employee = () => {
     const handleClose = () => {
         setOpen(false);
     };
-  
+
+    const [employee, setEmployee] = React.useState({
+        nom: "last name"
+    });
+
+    const handleNewEmployee = (newEmployee) => {
+        setEmployee({
+            name: newEmployee.target.value
+        });
+        employeesList.push(newEmployee.target.value);
+    };
+
+
     return (
         <div>
-        <Paper textAlign = "center" variant="outlined" style = {{
-            marginTop: "50px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            padding: "30px 45px 30px 45px",
-            width: "fit-content",
-            display: "block",
-            textAlign: "center"
-        }} >
-        <Typography variant="h2"> Manage employee 
-        <br />
-        <h5>Employees</h5>
-        <br />
-        </Typography>
-         <br />
-         <Button variant="contained" onClick={ handleClickOpen } >Add employee</Button>
-         <br /><br />
-        <Button variant="contained" >Save</Button>
-        <br />
-        </Paper>
-        
-        <div>
-        <Dialog open={open} onClose={handleClose}>
-            <DialogContent>
-            <DialogContentText>
-                Enter the employee's name
-            </DialogContentText>
-            <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                fullWidth
-                variant="standard"
-            />
-            </DialogContent>
-            <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose}>Add</Button>
-            </DialogActions>
-        </Dialog>
+            <Paper textAlign="center" variant="outlined" style={{
+                marginTop: "50px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                padding: "30px 45px 30px 45px",
+                width: "fit-content",
+                display: "block",
+                textAlign: "center"
+            }} >
+                <Typography variant="h2"> Manage employee
+                </Typography>
+                <br />
+
+                <Typography>
+                    <h5 style={{textAlign: "left"}}>
+                        Employees list
+                        <br />
+                        <ul>{employeesList}</ul>
+                       
+                    </h5>
+                </Typography>           
+
+                <Button variant="contained" onClick={handleClickOpen} >Add employee</Button>
+                <br /><br />
+                <Button variant="contained" >Save</Button>
+                <br />
+            </Paper>
+
+            <div>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogContent>
+                        <DialogContentText>
+                            Enter employee's name
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            fullWidth
+                            variant="standard"
+                            onChange={(handleNewEmployee)}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Add</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         </div>
-    </div>
     );
 };
 
 
-export default Employee;
+export default EmployeeManagement;
 
