227344674bfc2b7f2908a0e416ba5099fa4383b8
VincentPeer <vincent.peer@heig-vd.ch>
2022-09-05 09:38:04 +0200

    merging

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -20,13 +20,18 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
 
-const pages = [
+const token = getToken();
+
+const loggedPages = [
   {link: 'home', text: 'Home', icon: <HomeIcon />},
-  {link: 'manageEmployee', text: 'Employés', icon: <PeopleAltIcon />},
+  {link: 'manageEmployee', text: 'Employee', icon: <PeopleAltIcon />},
+  {link: 'profile', text: 'Profile'}
+
 ];
 
-const unLoggedPages = [{link: 'home', text: 'Home', icon: <InboxIcon />}]
+const unLoggedPages = [{link: 'home', text: 'Home', icon: <HomeIcon />}]
 
+const pages = token == null ? unLoggedPages : loggedPages;
 
 const Layout = () => {
   React.useEffect(() => {
